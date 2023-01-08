3748c14fa473676288b36f9dfe52c6a8ba3574c9
biancode <klaus@bianco-royal.de>
2023-01-08 10:41:47 +0100

    test(coverage): coverage command script

---
+++ jest.config.coverage.ts
@@ -0,0 +1,42 @@
+import type {Config} from 'jest';
+
+const config: Config = {
+  'verbose': true,
+  'transform': {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  'reporters': ['default', 'github-actions'],
+  'collectCoverageFrom': [
+    '**/lib/**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/dist/**'
+  ],
+  'coverageReporters': ['clover', 'json', 'lcov', ['text', {skipFull: true}]],
+  'coverageThreshold': {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10
+    }
+  },
+  'projects': [
+    {
+      displayName: {
+        color: 'green',
+        name: 'ts-test'
+      },
+      testMatch: ['<rootDir>/source/test/**/*.spec.ts']
+    },
+    {
+      displayName: {
+        color: 'blue',
+        name: 'types'
+      },
+      runner: 'jest-runner-tsd',
+      testMatch: ['<rootDir>/source/test/types/**/*.spec.ts']
+    }
+  ]
+};
+
+export default config;
